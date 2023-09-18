0e411b1038aaabd1c50b549dd44886aa7a097088
chadzusebmt <137036675+chadzusebmt@users.noreply.github.com>
2023-09-18 14:15:11 +0200

    section-2 video 13 -14

---
+++ src/components/ui/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+
+function Header() {
+  return(
+    <AppBar>
+        <Toolbar>
+            Logo
+        </Toolbar>
+    </AppBar>
+  )
+}
+
+export default Header;
