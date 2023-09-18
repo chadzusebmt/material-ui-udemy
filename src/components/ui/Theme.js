bffa79e8853b102a8af4f389f65b455cdd7e5e4f
chadzusebmt <137036675+chadzusebmt@users.noreply.github.com>
2023-09-18 15:16:16 +0200

    theme 18

--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -1,3 +1,19 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 
-export default createMuiTheme({});
+const arcBlue = "#0B72B9"
+const arcOrange = "#FFBA60"
+
+export default createMuiTheme({
+    palette: {
+        common: {
+            arcBlue: `${arcBlue}`,
+            arcOrange: `${arcOrange}`,
+        },
+        primary: {
+            main: `${arcBlue}`
+        },
+        secondary: {
+            main: `${arcOrange}`,
+        },
+    }
+});
