f868b4e6b2de1f3a2d93da95748e12a494145f75
chadzusebmt <137036675+chadzusebmt@users.noreply.github.com>
2023-09-18 15:08:29 +0200

    Theme-17

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,23 @@
 import Header from "./ui/Header";
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from "./ui/Theme"
 
 function App() {
   return (
-    <div className="App">
+    <ThemeProvider theme = {theme}>
+
     <Header />
-    </div>
+
+    {[...new Array(150)]
+            .map(
+              () => `Cras mattis consectetur purus sit amet fermentum.
+              Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+              Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
+              )
+              .join('\n')}
+
+              </ThemeProvider>
   );
 }
 
--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -1,14 +1,28 @@
 import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 
-function Header() {
+function ElevationScroll(props){
+    const {children} = props;
+    const trigger = useScrollTrigger({
+        disableHysteresis: true,
+        threshold: 100,
+    });
+
+    return React.cloneElement(children, {
+        elevation: trigger ? 4 : 0
+    })
+}
+
+
+function Header(props) {
   return(
-    <AppBar>
-        <Toolbar>
-            Logo
-        </Toolbar>
-    </AppBar>
+    <ElevationScroll>
+       <AppBar position="fixed">
+         <Toolbar>Logo</Toolbar>
+       </AppBar>
+    </ElevationScroll>
   )
 }
 
