3ce518c6ce103c26e7e45ab15a9096117bd71d82
chadzusebmt <137036675+chadzusebmt@users.noreply.github.com>
2023-09-21 09:28:56 +0200

    lesson 28 complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,26 @@
-import Header from "./ui/Header";
+import Header from './ui/Header';
 import { ThemeProvider } from '@material-ui/core/styles';
-import theme from "./ui/Theme"
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import theme from './ui/Theme';
 
 function App() {
   return (
-    <ThemeProvider theme = {theme}>
-
-    <Header />
-
-    {[...new Array(150)]
-            .map(
-              () => `Cras mattis consectetur purus sit amet fermentum.
-              Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-              Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
-              )
-              .join('\n')}
-
-              </ThemeProvider>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={() => <div> Home </div>} />
+          <Route exact path="/services" component={() => <div> Service </div>} />
+          <Route exact path="/customsoftware" component={() => <div> Custom Software </div>} />
+          <Route exact path="/mobileapps" component={() => <div> Mobile App </div>} />
+          <Route exact path="/websites" component={() => <div> Website </div>} />
+          <Route exact path="/revolution" component={() => <div> Revolution </div>} />
+          <Route exact path="/about" component={() => <div> About Us </div>} />
+          <Route exact path="/contact" component={() => <div> Contact Us </div>} />
+          <Route exact path="/estimate" component={() => <div> Estimate </div>} />
+        </Switch>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -1,75 +1,78 @@
-import React, { useState } from "react";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
+import React, { useState } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
-import { Button, Tab, Tabs, Typography } from "@material-ui/core";
+import { Button, Tab, Tabs, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
+import { Link } from 'react-router-dom';
 
+function ElevationScroll(props) {
+  const { children } = props;
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 100
+  });
 
-function ElevationScroll(props){
-    const {children} = props;
-    const trigger = useScrollTrigger({
-        disableHysteresis: true,
-        threshold: 100,
-    });
-
-    return React.cloneElement(children, {
-        elevation: trigger ? 4 : 0
-    })
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0
+  });
 }
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   //this will access the margin from the material theme and apply it on the toolbar
-  toolbarMargin:{
-    ...theme.mixins.toolbar,
+  toolbarMargin: {
+    ...theme.mixins.toolbar
   },
-  tabContainer:{
-    marginLeft: "auto"
+  tabContainer: {
+    marginLeft: 'auto'
   },
-  tab:{
+  tab: {
     ...theme.typography.tab,
     minWidth: 10,
-    marginLeft: "25px",
+    marginLeft: '25px'
   },
-  button:{
+  button: {
     ...theme.typography.estimate,
-    borderRadius: "50px",
-    marginLeft: "50px",
-    marginLeft: "25px",
-    height: "45px",
+    borderRadius: '50px',
+    marginLeft: '50px',
+    marginLeft: '25px',
+    height: '45px'
   }
 }));
 
 function Header(props) {
   const classes = useStyles();
 
-  const [value, setValue] = useState(0)
-  const handleChange = (e, value) => setValue(value)
-  
+  const [value, setValue] = useState(0);
+  const handleChange = (e, value) => setValue(value);
 
-  return(
+  return (
     <React.Fragment>
-    <ElevationScroll>
-       <AppBar position="fixed" color="primary">
-         <Toolbar disbaleGutters>
-          <Typography variant="h3" color="secondary"> Logo </Typography>  
+      <ElevationScroll>
+        <AppBar position="fixed" color="primary">
+          <Toolbar disbaleGutters>
+            <Typography variant="h3" color="secondary">
+              Logo
+            </Typography>
 
-          <Tabs value={value} onChange={handleChange} className={classes.tabContainer} indicatorColor="primary">
-            <Tab label="Home" className={classes.tab}></Tab>
-            <Tab label="Services" className={classes.tab}></Tab>
-            <Tab label="The revolution" className={classes.tab}></Tab>
-            <Tab label="About Us" className={classes.tab}></Tab>
-            <Tab label="Contact Us" className={classes.tab}></Tab>
-          </Tabs>
+            <Tabs value={value} onChange={handleChange} className={classes.tabContainer} indicatorColor="primary">
+              <Tab label="Home" className={classes.tab} component={Link} to="/" />
+              <Tab label="Services" className={classes.tab} component={Link} to="/services" />
+              <Tab label="The revolution" className={classes.tab} component={Link} to="/revolution" />
+              <Tab label="About Us" className={classes.tab} component={Link} to="/about" />
+              <Tab label="Contact Us" className={classes.tab} component={Link} to="/contact" />
+            </Tabs>
 
-          <Button variant="contained" color="secondary" className={classes.button}> Free Estimate</Button>
-
-        </Toolbar>
-       </AppBar>
-    </ElevationScroll>
-    <div className={classes.toolbarMargin }/>
+            <Button variant="contained" color="secondary" className={classes.button}>
+              {' '}
+              Free Estimate
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <div className={classes.toolbarMargin} />
     </React.Fragment>
-  )
+  );
 }
 
 export default Header;
