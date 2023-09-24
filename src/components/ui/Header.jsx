b9e6d5e4f3871b9cf4c3039ff9a10cf14aeb6e73
chadzusebmt <137036675+chadzusebmt@users.noreply.github.com>
2023-09-24 17:13:45 +0200

    lesson 38 completed

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -82,19 +82,19 @@ function Header(props) {
 
   // this persist the tab to be hilighted even if the user refresh the page
   useEffect(() => {
-    if (window.location.pathname === '/' && value !== 0) {
-      setValue(0);
-    } else if (window.location.pathname === '/services' && value !== 1) {
-      setValue(1);
-    } else if (window.location.pathname === '/revolution' && value !== 2) {
-      setValue(2);
-    } else if (window.location.pathname === '/about' && value !== 3) {
-      setValue(3);
-    } else if (window.location.pathname === '/contact' && value !== 4) {
-      setValue(4);
-    } else if (window.location.pathname === '/estimate' && value !== 5) {
-      setValue(5);
-    }
+    // if (window.location.pathname === '/' && value !== 0) {
+    //   setValue(0);
+    // } else if (window.location.pathname === '/services' && value !== 1) {
+    //   setValue(1);
+    // } else if (window.location.pathname === '/revolution' && value !== 2) {
+    //   setValue(2);
+    // } else if (window.location.pathname === '/about' && value !== 3) {
+    //   setValue(3);
+    // } else if (window.location.pathname === '/contact' && value !== 4) {
+    //   setValue(4);
+    // } else if (window.location.pathname === '/estimate' && value !== 5) {
+    //   setValue(5);
+    // }
 
     switch (window.location.pathname) {
       case '/':
