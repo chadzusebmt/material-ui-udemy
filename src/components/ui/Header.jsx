df18e96817f9a3be32e321427a66191d1157d3de
chadzusebmt <137036675+chadzusebmt@users.noreply.github.com>
2023-09-26 19:19:35 +0200

    Lesson 46 completed

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -228,7 +228,8 @@ function Header(props) {
         open={openDrawer}
         onClose={() => setOpenDrawer(false)}
         onOpen={() => setOpenDrawer(true)}
-        // classes={{ paper: classes.drawer }}
+        // Using this method will overwrite the style that comes with material so that you can apply your own style
+        classes={{ paper: classes.drawer }} // i create my own stle here
       >
         {/* <div className={classes.toolbarMargin} /> */}
         {/* <List disablePadding>
@@ -330,22 +331,41 @@ function Header(props) {
 
         <List disablePadding>
           <ListItem onClick={() => setOpenDrawer(false)} divider button component={Link} to="/">
-            <ListItemText disableTypography>Home</ListItemText>
+            <ListItemText className={classes.drawerItem} disableTypography>
+              Home
+            </ListItemText>
           </ListItem>
           <ListItem onClick={() => setOpenDrawer(false)} divider button component={Link} to="/services">
-            <ListItemText disableTypography>Services</ListItemText>
+            <ListItemText className={classes.drawerItem} disableTypography>
+              Services
+            </ListItemText>
           </ListItem>
           <ListItem onClick={() => setOpenDrawer(false)} divider button component={Link} to="/revolution">
-            <ListItemText disableTypography>The Revolution</ListItemText>
+            <ListItemText className={classes.drawerItem} disableTypography>
+              The Revolution
+            </ListItemText>
           </ListItem>
           <ListItem onClick={() => setOpenDrawer(false)} divider button component={Link} to="/about">
-            <ListItemText disableTypography>About Us</ListItemText>
+            <ListItemText className={classes.drawerItem} disableTypography>
+              About Us
+            </ListItemText>
           </ListItem>
           <ListItem onClick={() => setOpenDrawer(false)} divider button component={Link} to="/contact">
-            <ListItemText disableTypography>Contact Us</ListItemText>
+            <ListItemText className={classes.drawerItem} disableTypography>
+              Contact Us
+            </ListItemText>
           </ListItem>
-          <ListItem onClick={() => setOpenDrawer(false)} divider button component={Link} to="/estimate">
-            <ListItemText disableTypography>Free Estimate</ListItemText>
+          <ListItem
+            onClick={() => setOpenDrawer(false)}
+            divider
+            button
+            component={Link}
+            to="/estimate"
+            className={classes.drawerItemEstimate}
+          >
+            <ListItemText className={[classes.drawerItem, classes.className]} disableTypography>
+              Free Estimate
+            </ListItemText>
           </ListItem>
         </List>
       </SwipeableDrawer>
@@ -459,5 +479,16 @@ const useStyles = makeStyles((theme) => ({
   drawerIcon: {
     height: '50px',
     width: '50px'
+  },
+  drawer: {
+    backgroundColor: theme.palette.common.blue
+  },
+  drawerItem: {
+    ...theme.typography.tab,
+    color: 'white',
+    opacity: 0.7
+  },
+  drawerItemEstimate: {
+    backgroundColor: theme.palette.common.orange
   }
 }));
--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -1,39 +1,37 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 
-const arcBlue = "#0B72B9"
-const arcOrange = "#FFBA60"
+const arcBlue = '#0B72B9';
+const arcOrange = '#FFBA60';
 
 export default createMuiTheme({
-    palette: {
-        common: {
-            arcBlue: `${arcBlue}`,
-            arcOrange: `${arcOrange}`,
-        },
-        primary: {
-            main: `${arcBlue}`
-        },
-        secondary: {
-            main: `${arcOrange}`,
-        },
+  palette: {
+    common: {
+      blue: `${arcBlue}`,
+      orange: `${arcOrange}`
+    },
+    primary: {
+      main: `${arcBlue}`
+    },
+    secondary: {
+      main: `${arcOrange}`
+    },
 
-        
-        typography:{
-            // these are custom changes for my typography styles
-            h3:{
-                fontWeight: '3000',
-            },
-            tab:{
-                fontWeight: '700',
-                fontSize: "1rem",
-                textTransform: "none",
-            },
-            estimate:{
-                fontFamily: "Pacifico",
-                fontSize: "1rem",
-                textTransform: "none",
-                color: "white",
-            }
-           
-        }
+    typography: {
+      // these are custom changes for my typography styles
+      h3: {
+        fontWeight: '3000'
+      },
+      tab: {
+        fontWeight: '700',
+        fontSize: '1rem',
+        textTransform: 'none'
+      },
+      estimate: {
+        fontFamily: 'Pacifico',
+        fontSize: '1rem',
+        textTransform: 'none',
+        color: 'white'
+      }
     }
+  }
 });
